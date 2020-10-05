677eb117d5569f7e7c3ca0521296cd82133c7e36
cpphoo <cp598@cornell.edu>
2020-10-05 19:06:17 -0400

    modified the block size for the copy optimization studies

--- dgemm_blocked_copy.c
+++ dgemm_blocked_copy.c
@@ -3,7 +3,7 @@ const char* dgemm_desc = "My awesome dgemm.";
 #include <string.h>
 
 #ifndef BLOCK_SIZE
-#define BLOCK_SIZE 64
+#define BLOCK_SIZE 128
 #endif
 
 #define min(a,b) (((a)<(b))?(a):(b))
