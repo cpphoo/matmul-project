a62c34e91d174cc6d1bfe831d431f127d6a41bbc
cpphoo <cp598@cornell.edu>
2020-10-05 18:46:59 -0400

    dgemm final version

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -2,75 +2,68 @@ const char *dgemm_desc = "My awesome dgemm.";
 #include <immintrin.h>
 #include <string.h>
 
-int block_m = 64;
-int block_n = 64;
-int block_k = 256;
-
-/* Block sizes */
-#define BLOCK_M block_m
-#define BLOCK_N block_n
-#define BLOCK_K block_k
-
-#define SMALL_MATRIX_DIM 150
+#ifndef BLOCK_SIZE
+#define BLOCK_SIZE 128
+#endif
 
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 #pragma GCC optimize("O3")
 
 /*
- * Copy (num_row, num_col) sub-matrix from source matrix of dim (src_m, src_n)
- * to destination matrix of dim (dest_m, dest_n).
- * Zero padding if (num_row, num_col) does not fill entire destination and not in reverse mode.
+ * Copy n_row-by-n_col submatrix from source matrix of size src_size
+ * to destination matrix of size dest_size. Both matrices are square.
+ * Zero padding if n_row or n_col does not fill entire block_size.
  */
-void matrix_copy_aligned(double *restrict destination, const double *restrict source, int dest_m, int dest_n,
-                         int src_m, int src_n, int num_row, int num_col, int reverse)
+void matrix_copy_aligned(double *restrict destination, const double *restrict source, int dest_size, int src_size, int n_row, int n_col, int block_size)
 {
-    if (!reverse && (num_row < dest_m || num_col < dest_n))
+    if (n_row < block_size || n_col < block_size)
     {
-        memset(destination, 0, dest_m * dest_n * sizeof(double));
+        memset(destination, 0, block_size * block_size * sizeof(double));
     }
-#pragma GCC unroll 4
-    for (int j = 0; j < num_col; ++j)
+
+    for (int j = 0; j < n_col; ++j)
     {
-        memcpy(&destination[j * dest_m], &source[j * src_m], num_row * sizeof(double));
+        memcpy(&destination[j * dest_size], &source[j * src_size], n_row * sizeof(double));
     }
 }
 
 /*
- * Performs matrix multiplication of
+ * Performs block matrix multiplication of
  *  C := C + A * B
  * where C is MM-by-NN, A is MM-by-KK, and B is KK-by-NN
  * using AVX instructions.
+ * TODO: Implement outer product trick introduced in http://web.mit.edu/neboat/www/6.S898-sp17/mm.pdf.
  */
-void AVX_matrix_multiply(double *restrict A, double *restrict B, double *restrict C, int MM, int NN, int KK)
+void AVX_matrix_block_multiply(double *restrict A, double *restrict B, double *restrict C, int MM, int NN, int KK, int block_size)
 {
     int i, j, k;
     __m256d c[8], left[2], right[4];
-
 #pragma GCC unroll 2
     for (i = 0; i < MM; i += 8)
     {
 #pragma GCC unroll 2
+
         for (j = 0; j < NN; j += 4)
         {
-            c[0] = _mm256_load_pd(&C[i + j * BLOCK_M]);
-            c[1] = _mm256_load_pd(&C[i + (j + 1) * BLOCK_M]);
-            c[2] = _mm256_load_pd(&C[i + (j + 2) * BLOCK_M]);
-            c[3] = _mm256_load_pd(&C[i + (j + 3) * BLOCK_M]);
-            c[4] = _mm256_load_pd(&C[i + 4 + j * BLOCK_M]);
-            c[5] = _mm256_load_pd(&C[i + 4 + (j + 1) * BLOCK_M]);
-            c[6] = _mm256_load_pd(&C[i + 4 + (j + 2) * BLOCK_M]);
-            c[7] = _mm256_load_pd(&C[i + 4 + (j + 3) * BLOCK_M]);
+            c[0] = _mm256_load_pd(&C[i + j * block_size]);
+            c[1] = _mm256_load_pd(&C[i + (j + 1) * block_size]);
+            c[2] = _mm256_load_pd(&C[i + (j + 2) * block_size]);
+            c[3] = _mm256_load_pd(&C[i + (j + 3) * block_size]);
+            c[4] = _mm256_load_pd(&C[i + 4 + j * block_size]);
+            c[5] = _mm256_load_pd(&C[i + 4 + (j + 1) * block_size]);
+            c[6] = _mm256_load_pd(&C[i + 4 + (j + 2) * block_size]);
+            c[7] = _mm256_load_pd(&C[i + 4 + (j + 3) * block_size]);
 
 #pragma GCC unroll 8
             for (k = 0; k < KK; ++k)
             {
-                left[0] = _mm256_load_pd(&A[i + k * BLOCK_M]);
-                left[1] = _mm256_load_pd(&A[i + 4 + k * BLOCK_M]);
+                left[0] = _mm256_load_pd(&A[i + k * block_size]);
+                left[1] = _mm256_load_pd(&A[i + 4 + k * block_size]);
 
-                right[0] = _mm256_broadcast_sd(&B[k + j * BLOCK_K]);
-                right[1] = _mm256_broadcast_sd(&B[k + (j + 1) * BLOCK_K]);
-                right[2] = _mm256_broadcast_sd(&B[k + (j + 2) * BLOCK_K]);
-                right[3] = _mm256_broadcast_sd(&B[k + (j + 3) * BLOCK_K]);
+                right[0] = _mm256_broadcast_sd(&B[k + j * block_size]);
+                right[1] = _mm256_broadcast_sd(&B[k + (j + 1) * block_size]);
+                right[2] = _mm256_broadcast_sd(&B[k + (j + 2) * block_size]);
+                right[3] = _mm256_broadcast_sd(&B[k + (j + 3) * block_size]);
 
                 c[0] = _mm256_fmadd_pd(left[0], right[0], c[0]);
                 c[1] = _mm256_fmadd_pd(left[0], right[1], c[1]);
@@ -81,61 +74,43 @@ void AVX_matrix_multiply(double *restrict A, double *restrict B, double *restric
                 c[6] = _mm256_fmadd_pd(left[1], right[2], c[6]);
                 c[7] = _mm256_fmadd_pd(left[1], right[3], c[7]);
             }
-            _mm256_storeu_pd(&C[i + j * BLOCK_M], c[0]);
-            _mm256_storeu_pd(&C[i + (j + 1) * BLOCK_M], c[1]);
-            _mm256_storeu_pd(&C[i + (j + 2) * BLOCK_M], c[2]);
-            _mm256_storeu_pd(&C[i + (j + 3) * BLOCK_M], c[3]);
-            _mm256_storeu_pd(&C[i + 4 + j * BLOCK_M], c[4]);
-            _mm256_storeu_pd(&C[i + 4 + (j + 1) * BLOCK_M], c[5]);
-            _mm256_storeu_pd(&C[i + 4 + (j + 2) * BLOCK_M], c[6]);
-            _mm256_storeu_pd(&C[i + 4 + (j + 3) * BLOCK_M], c[7]);
+            _mm256_storeu_pd(&C[i + j * block_size], c[0]);
+            _mm256_storeu_pd(&C[i + (j + 1) * block_size], c[1]);
+            _mm256_storeu_pd(&C[i + (j + 2) * block_size], c[2]);
+            _mm256_storeu_pd(&C[i + (j + 3) * block_size], c[3]);
+            _mm256_storeu_pd(&C[i + 4 + j * block_size], c[4]);
+            _mm256_storeu_pd(&C[i + 4 + (j + 1) * block_size], c[5]);
+            _mm256_storeu_pd(&C[i + 4 + (j + 2) * block_size], c[6]);
+            _mm256_storeu_pd(&C[i + 4 + (j + 3) * block_size], c[7]);
         }
     }
 }
 
 /*
- * Performs matrix multiplication of
- *  C := C + A * B
- * where C is MM-by-NN, A is MM-by-KK, and B is KK-by-NN
- * using sub-blocking plus AVX.
  */
-void square_dgemm(const int dim, const double *A, const double *B, double *C)
+void square_dgemm(const int N, const double *A, const double *B, double *C)
 {
-    if (dim < SMALL_MATRIX_DIM)
-    {
-        BLOCK_M = 32;
-        BLOCK_N = 32;
-        BLOCK_K = 32;
-    }
-    else
-    {
-        BLOCK_M = 64;
-        BLOCK_N = 64;
-        BLOCK_K = 256;
-    }
-
-    double *subA = (double *)_mm_malloc(BLOCK_M * BLOCK_K * sizeof(double), 64);
-    double *subB = (double *)_mm_malloc(BLOCK_K * BLOCK_N * sizeof(double), 64);
-    double *subC = (double *)_mm_malloc(BLOCK_M * BLOCK_N * sizeof(double), 64);
+    double *subA = (double *)_mm_malloc(BLOCK_SIZE * BLOCK_SIZE * sizeof(double), 64);
+    double *subB = (double *)_mm_malloc(BLOCK_SIZE * BLOCK_SIZE * sizeof(double), 64);
+    double *subC = (double *)_mm_malloc(BLOCK_SIZE * BLOCK_SIZE * sizeof(double), 64);
     int i, j, k;
-    int dim_i, dim_j, dim_k;
-    for (i = 0; i < dim; i += BLOCK_M)
+    for (i = 0; i < N; i += BLOCK_SIZE)
     {
-        dim_i = min(BLOCK_M, (dim - i));
+        int dim_i = min(BLOCK_SIZE, (N - i));
 #pragma GCC ivdep
 #pragma GCC unroll 2
-        for (j = 0; j < dim; j += BLOCK_N)
+        for (j = 0; j < N; j += BLOCK_SIZE)
         {
-            dim_j = min(BLOCK_N, (dim - j));
-            matrix_copy_aligned(subC, &C[i + j * dim], BLOCK_M, BLOCK_N, dim, dim, dim_i, dim_j, 0);
-            for (k = 0; k < dim; k += BLOCK_K)
+            int dim_j = min(BLOCK_SIZE, (N - j));
+            matrix_copy_aligned(subC, &C[i + j * N], BLOCK_SIZE, N, dim_i, dim_j, BLOCK_SIZE);
+            for (k = 0; k < N; k += BLOCK_SIZE)
             {
-                dim_k = min(BLOCK_K, (dim - k));
-                matrix_copy_aligned(subA, &A[i + k * dim], BLOCK_M, BLOCK_K, dim, dim, dim_i, dim_k, 0);
-                matrix_copy_aligned(subB, &B[k + j * dim], BLOCK_K, BLOCK_N, dim, dim, dim_k, dim_j, 0);
-                AVX_matrix_multiply(subA, subB, subC, dim_i, dim_j, dim_k);
+                int dim_k = min(BLOCK_SIZE, (N - k));
+                matrix_copy_aligned(subA, &A[i + k * N], BLOCK_SIZE, N, dim_i, dim_k, BLOCK_SIZE);
+                matrix_copy_aligned(subB, &B[k + j * N], BLOCK_SIZE, N, dim_k, dim_j, BLOCK_SIZE);
+                AVX_matrix_block_multiply(subA, subB, subC, dim_i, dim_j, dim_k, BLOCK_SIZE);
             }
-            matrix_copy_aligned(&C[i + j * dim], subC, dim, dim, BLOCK_M, BLOCK_N, dim_i, dim_j, 1);
+            matrix_copy_aligned(&C[i + j * N], subC, N, BLOCK_SIZE, dim_i, dim_j, 0);
         }
     }
 
@@ -143,23 +118,3 @@ void square_dgemm(const int dim, const double *A, const double *B, double *C)
     _mm_free(subB);
     _mm_free(subC);
 }
-
-/*
- * TODO 1: Try different block sizes (not necessarily square) for A, B and C to increase L1/L2 cache hits.
- * TODO 2: Transpose A/B to row major.
- */
-// void square_dgemm(const int dim, const double *A, const double *B, double *C)
-// {
-//     if (dim < SMALL_MATRIX_DIM)
-//     {
-//         BLOCK_M = 32;
-//         BLOCK_N = 32;
-//         BLOCK_K = 32;
-//     }
-//     else {
-//         BLOCK_M = 64;
-//         BLOCK_N = 64;
-//         BLOCK_K = 256;
-//     }
-//     block_matrix_multiply(dim, A, B, C);
-// }
